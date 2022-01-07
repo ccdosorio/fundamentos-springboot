482e2e0407d62dd4633ac4a92a275d830eda849e
Christian Osorio <ccdosorio11@gmail.com>
2022-01-07 16:35:19 -0600

    Modelando entidades con JPA

---
+++ src/main/java/com/fundamentos/springboot/fundamentos/entity/Post.java
@@ -0,0 +1,14 @@
+package com.fundamentos.springboot.fundamentos.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "post")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_post")
+    private Long id;
+
+
+}
